9a52d1f13095daae8e877bd31eb50aac1485f343
Andy Irving <mahrev24@gmail.com>
2021-02-27 17:54:33 -0600

    Update problemK.py
    just a start to the code before the competition ran out, doesn't actually work :)

--- problemK.py
+++ problemK.py
@@ -1,1 +1,15 @@
+import sys
 
+icpc = []
+outside = []
+mismatches = True
+res = []
+
+for i in sys.stdin:
+	
+	lin = i.split()
+	if mismatches == True:
+		for x in res:
+			print(res) 
+	else:
+		print("No mismatches.")
